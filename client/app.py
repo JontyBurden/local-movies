84bf7a3c1265f6bf7803cb9b62b8346e3cd12f45
JontyBurden <azttorrs@gmail.com>
2021-06-30 16:35:47 +1200

    tested front end display with API and Flask

--- client/app.py
+++ client/app.py
@@ -3,27 +3,60 @@ import requests
 
 app = Flask(__name__)
 
+#test cases for API matching and front-end display
+movie_titles = [
+    {
+    "year": 2014,
+    "quality": "HDRip",
+    "codec": "XViD",
+    "title": "Dawn+of+the+Planet+of+the+Apes"
+    },
+    {
+    'group': 'JYK',
+    'title': 'San+Andreas',
+    'resolution': '720p',
+    'codec': 'x264',
+    'year':  '2015',
+    'audio': 'AAC',
+    'quality': 'WEB-DL'
+    }
+  ]
+
 @app.route('/movie', methods=['POST'])
-def MovieDetails():
-    #img_src = 'https://image.tmdb.org/t/p/w500'
-    r = 'https://api.themoviedb.org/3/search/movie?api_key=4cc1b68a07fe5ba265950e85ac96cb2c&query=Jack+Reacher'
-    movie_req = requests.get(r)
-    d = movie_req.json()
+def MovieDetails():     
+    """
+    takes db qry of all movies and handles API requests for each matching both movie tile and year
+    """
     movieDetails = []
-    for key, value in d.items():
-        if key == 'results':
-            movieDetails = value
-    movieTitle = movieDetails[0]['title']
-    movieDescp = movieDetails[0]['overview']
-    movieRating = movieDetails[0]['vote_average']
-    #movieImage = img_src + MovieDetails[0]['poster_path']
-    return render_template('movie.html', 
-    movieTitle=movieTitle,     
-    movieDescp=movieDescp,
-    movieRating=movieRating)
+    for dic in movie_titles:            
+        for key in dic:                  
+                if dic[key] == dic['title']:                    
+                    movie_req = 'https://api.themoviedb.org/3/search/movie?api_key=4cc1b68a07fe5ba265950e85ac96cb2c&query={}&year={}'.format(dic['title'],dic['year'])
+                    r = requests.get(movie_req)
+                    movie_detials = r.json()
+                    
+                    # selection of key and values
+                    test = []
+                    for key, value in movie_detials.items():
+                        if key == 'results':
+                            test = value
+
+                    # parsing movieDetials 
+                    movieTitle = next((item['title'] for item in test), None)   
+                    movieRating = next((item['vote_average'] for item in test), None)
+                    movieYear = next((item['release_date'] for item in test), None)
+                    movieOverview = next((item['overview'] for item in test), None)
+                    
+                    # creating a list of nested dict for movieDetials
+                    movie_dict = {"title": movieTitle, "year": movieYear, "rating": movieRating, "overview": movieOverview}
+                    movieDetails.append(movie_dict.copy())
+                          
+    return render_template('movie.html',
+    movieDetails=movieDetails)
 
 @app.route('/')
 def index():
+
 	return render_template('index.html')
 
 if __name__ == '__main__':
--- client/test.py
+++ client/test.py
@@ -1,22 +1,73 @@
-import requests, json
+import requests, json, pprint
 
+"""
+place for testing features
 
+"""
 r  = 'https://api.themoviedb.org/3/search/movie?api_key=4cc1b68a07fe5ba265950e85ac96cb2c&query=Jack+Reacher'
 
-movie_req = requests.get(r)
-data = movie_req.json()
-
-movieTitle = data.get('result', {})
-
+movie_titles = [
+    {
+    "year": 2014,
+    "quality": "HDRip",
+    "codec": "XViD",
+    "title": "Dawn+of+the+Planet+of+the+Apes"
+    },
+    {
+    'group': 'JYK',
+    'title': 'San+Andreas',
+    'resolution': '720p',
+    'codec': 'x264',
+    'year':  '2015',
+    'audio': 'AAC',
+    'quality': 'WEB-DL'
+    }
+  ]
 
 d = {"page":1,"results":
 [{"adult":"false","backdrop_path":"/k7h4RNAarfOrF2r2YMN0P2FQSr4.jpg","genre_ids":[80,18,53,28],"id":75780,"original_language":"en","original_title":"Jack Reacher","poster_path":"/zlyhKMi2aLk25nOHnNm43MpZMtQ.jpg","release_date":"2012-12-20","title":"Jack Reacher"}], "total_pages":1,"total_results":2}
 
-test = []
-for key, value in d.items():
-    if key == 'results':
-        test = value
+def getMovieDetails(movie_titles):
+      
+    """
+    takes db qry of all movies and handles API requests for each matching both movie tile and year
+    """
+    movieDetails = []
+    for dic in movie_titles:            
+        for key in dic:                  
+                if dic[key] == dic['title']:                    
+                    movie_req = 'https://api.themoviedb.org/3/search/movie?api_key=4cc1b68a07fe5ba265950e85ac96cb2c&query={}&year={}'.format(dic['title'],dic['year'])
+                    r = requests.get(movie_req)
+                    movie_detials = r.json()
+                    #print(movie_detials)
+                    # selection of key and values
+                    test = []
+                    for key, value in movie_detials.items():
+                        if key == 'results':
+                            test = value
 
-img_src = 'https://image.tmdb.org/t/p/w500'
-print(img_src + test[0]['poster_path'])
+                    movieTitle = next((item['title'] for item in test), None)
+    
+                    movieRating = next((item['vote_average'] for item in test), None)
+
+                    movieYear = next((item['release_date'] for item in test), None)
+
+                    movieOverview = next((item['overview'] for item in test), None)
+                    
+
+                    
+                    movie_dict = {"title": movieTitle, "year": movieYear, "movieRating": movieRating, "overview": movieOverview}
+                    movieDetails.append(movie_dict.copy())
+    #print(movieDetails)
+    print(movieDetails)
+        
+    
+    
+    #print(titles)
+
+                    
+    
+getMovieDetails(movie_titles)
+
+
 
